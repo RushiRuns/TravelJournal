0f945cbd652a0f43d7318f7bcf20dc0e175df74f
RushiRuns <rushikeshaundhakar715@gmail.com>
2024-10-09 02:50:23 +0530

    created list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
 import List from "./components/List";
+import Data from "./data";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
-      <List />
+      <section>
+        {Data.map((data) => (
+          <List key={data.id} {...data} />
+        ))}
+      </section>
     </div>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,39 @@
-import Data from "../data";
+import location from "/images/location.svg";
 
-export default function List() {
-  return <section></section>;
+export default function List(props) {
+  return (
+    <div>
+      <div className="travel-list">
+        <div className="travel-list__img">
+          <img
+            src={`/images/${props.imageUrl}`}
+            alt=""
+            className="travel-img"
+          />
+          <div className="travel-location">
+            <img src={location} alt="" className="location-logo" />
+            <span className="location-name">{props.location} </span>
+          </div>
+        </div>
+        <div className="travel-info">
+          <h1 className="travel-info__heading">{props.title}</h1>
+          <div>
+            <span className="travel-time">
+              <small>{props.startDate} - </small>{" "}
+            </span>
+            <span className="travel-time">
+              <small>{props.endDate}</small>
+            </span>
+          </div>
+          <p className="travel-desc">{props.description}</p>
+          <a href={props.googleMapsUrl} className="gmap-link">
+            View on Google Maps ↗
+          </a>
+        </div>
+      </div>
+      <div className="hr-holder">
+        <hr className="hr" />
+      </div>
+    </div>
+  );
 }
