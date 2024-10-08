28b9d0f845abfda9ade075b0e6d96ff99cd55768
RushiRuns <rushikeshaundhakar715@gmail.com>
2024-10-09 01:09:15 +0530

    created the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import img from "/images/path.svg";
 export default function Navbar() {
   return (
-    <header>
-      <nav></nav>
+    <header className="nav-container">
+      <nav className="nav">
+        <img src={img} alt="" className="nav__logo" />
+        <p className="nav__title">my travel journal.</p>
+      </nav>
     </header>
   );
 }
